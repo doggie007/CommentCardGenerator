72dc4a0f07642991085767688f6f36a6e30e5195
James Tcheng <james.tcheng1234@gmail.com>
2022-02-04 11:58:19 +0000

    Initial Commit

---
+++ CommentCardGeneratorApp/CommentCardGeneratorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardGeneratorAppApp.swift
+//  CommentCardGeneratorApp
+//
+//  Created by Tcheng, James (BJH) on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardGeneratorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
